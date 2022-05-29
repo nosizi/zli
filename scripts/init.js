a1387c043032585d9c8dd22927c0ee89182b4d88
nosizi <nosizi@163.com>
2022-05-30 00:03:54 +0800

    feat: execute install at the end

--- scripts/init.js
+++ scripts/init.js
@@ -1,58 +1,55 @@
-const inquirer = require('inquirer')
-const fs = require('fs')
-const path = require('path')
-const execa = require('execa')
-
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const execa = require("execa");
 
 function getTargetPkgJsonOnParse() {
   return JSON.parse(
-    fs.readFileSync(path.resolve(
-      process.cwd(),
-      './package.json',
-    ), {
-      encoding: 'utf8',
+    fs.readFileSync(path.resolve(process.cwd(), "./package.json"), {
+      encoding: "utf8",
     })
-  )
+  );
 }
 
 function getTsDependencies(isNode) {
-  const targetPkgFile = getTargetPkgJsonOnParse()
-  const { dependencies, devDependencies } = targetPkgFile
+  const targetPkgFile = getTargetPkgJsonOnParse();
+  const { dependencies, devDependencies } = targetPkgFile;
 
-  const depArgs = []
-  const devDepArgs = []
-  const availableTs = dependencies && Reflect.has(dependencies, 'typescript')
-  const availableNodeTypes = devDependencies && Reflect.has(devDependencies, '@types/node')
+  const depArgs = [];
+  const devDepArgs = [];
+  const availableTs = dependencies && Reflect.has(dependencies, "typescript");
+  const availableNodeTypes =
+    devDependencies && Reflect.has(devDependencies, "@types/node");
   console.log({ isNode });
 
   if (isNode) {
     if (!availableNodeTypes) {
-      devDepArgs.push('@types/node')
+      devDepArgs.push("@types/node");
     }
   }
 
   if (!availableTs) {
-    depArgs.push('typescript')
+    depArgs.push("typescript");
   }
 
   return {
     depArgs,
     devDepArgs,
-  }
+  };
 }
 
 function setTsConfigFile(flag) {
   const configString = fs.readFileSync(
     path.resolve(__dirname, `../lib/config/${flag}.tsconfig.json`),
     {
-      encoding: 'utf8',
+      encoding: "utf8",
     }
-  )
+  );
   fs.writeFileSync(
-    path.resolve(process.cwd(), './tsconfig.json'),
+    path.resolve(process.cwd(), "./tsconfig.json"),
     configString,
-    { encoding: 'utf8' }
-  )
+    { encoding: "utf8" }
+  );
 }
 
 function initAction() {
@@ -69,44 +66,46 @@ function initAction() {
       //   message: 'lint-staged?',
       // },
       {
-        type: 'list',
-        name: 'tsconfig',
-        message: 'tsconfig.json? Choose and install typescript or not. If your dependencies doesn\'t have typescript and it will be install.',
-        choices: ['No', 'JavaScript', 'Node'],
-        default: 'No',
+        type: "list",
+        name: "tsconfig",
+        message:
+          "tsconfig.json? Choose and install typescript or not. If your dependencies doesn't have typescript and it will be install.",
+        choices: ["No", "JavaScript", "Node"],
+        default: "No",
       },
     ])
     .then(async (answers) => {
-      const { tsconfig } = answers
+      const { tsconfig, husky } = answers;
 
-      let tsDeps = {}
-      if (tsconfig && tsconfig !== 'No') {
-        tsDeps = getTsDependencies(tsconfig === 'Node')
+      let finalDeps = [];
+      let finalDevDeps = [];
+
+      if (husky) {
+        finalDevDeps = ["husky"];
+      }
+
+      let tsDeps = {};
+      if (tsconfig && tsconfig !== "No") {
+        tsDeps = getTsDependencies(tsconfig === "Node");
       }
       console.log(tsDeps);
 
-      if (
-        Reflect.has(tsDeps, 'depArgs')
-        && tsDeps.depArgs.length
-      ) {
-        await execa('npm', [
-          'install',
-          '--save',
-          ...tsDeps.depArgs,
-        ])
+      if (Reflect.has(tsDeps, "depArgs") && tsDeps.depArgs.length) {
+        finalDeps = [...finalDeps, ...tsDeps.depArgs];
       }
 
-      if (
-        Reflect.has(tsDeps, 'devDepArgs')
-        && tsDeps.devDepArgs.length
-      ) {
-        await execa('npm', [
-          'install',
-          '--save-dev',
-          ...tsDeps.devDepArgs,
-        ])
+      if (Reflect.has(tsDeps, "devDepArgs") && tsDeps.devDepArgs.length) {
+        finalDevDeps = [...finalDevDeps, ...tsDeps.devDepArgs];
       }
-    })
+
+      if (finalDeps.length) {
+        await execa("npm", ["install", "--save", ...finalDeps]);
+      }
+
+      if (finalDevDeps.length) {
+        await execa("npm", ["install", "--save-dev", ...finalDevDeps]);
+      }
+    });
 }
 
-module.exports = initAction
+module.exports = initAction;
