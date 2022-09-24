32469d4da7232c2a49e869af31a56223d6f03dbb
nosizi <nosizi@163.com>
2022-09-25 01:35:52 +0800

    feat: install function, change local's config file

--- scripts/init.js
+++ scripts/init.js
@@ -3,13 +3,18 @@ const fs = require("fs");
 const path = require("path");
 const { Buffer } = require('buffer')
 const execa = require("execa");
+const spawn = require('cross-spawn')
 const chalk = require('chalk');
+const stringify = require('json-stable-stringify-without-jsonify')
 
-const eslintLibConfig = require('../lib/eslintrc')
+// config files
+const eslintLibConfig = require('../lib/eslintrc');
+const stylelintLibConfig = require('../lib/stylelint')
+const prettierLibConfig = require('../lib/prettier.json')
+const editorConfig = require('../lib/editorconfig')
 
 const pkgFilePath = path.resolve(process.cwd(), "./package.json")
 const hasPackageJson = fs.existsSync(pkgFilePath)
-
 
 
 let theAnswer = {
@@ -53,6 +58,8 @@ class Initial {
     const { stylelint } = await this.getAskStylelint()
     const { prettier } = await this.getAskPrettier()
     const { husky } = await this.getAskHusky()
+    const { editorconfig } = await this.getAskEditorConfig()
+    const { pkgManager } = await this.getAskPackageManager()
 
     // set all answer...
     this.theAnswer = {
@@ -63,6 +70,7 @@ class Initial {
       stylelint,
       prettier,
       husky,
+      pkgManager,
     }
 
     if (framework === 'React') this.isReact = true
@@ -76,15 +84,53 @@ class Initial {
     if (eslint) {
       this.generateESLintDeps()
     }
+
+    if (stylelint) {
+      this.generateStylelintDeps()
+    }
+
+    if (prettier) {
+      this.generatePrettierDeps()
+    }
     console.log(this.dependencies);
+
+    // write dependencies to package.json
+    // this.writePackageJSON()
+
+    // install dependencies
+    this.installDependencies()
 
     // write files in the end
     if (eslint) {
       this.writeESLintConfigFile()
     }
-    // if (typescript) {
-    //   this.writeTSConfigFile()
-    // }
+    if (typescript) {
+      this.writeTSConfigFile()
+    }
+    if (prettier) {
+      this.writePrettierConfigFile()
+    }
+    if (stylelint) {
+      this.writeStylelintConfigFile()
+    }
+    if (editorconfig) {
+      this.writeEditorConfigFile()
+    }
+    console.log('All config files has been created!');
+    console.log('You can install it all now by manual.');
+  }
+
+  static async installDependencies() {
+    const { devDeps } = this.dependencies
+    const { pkgManager } = this.theAnswer
+    const installCommand = pkgManager === 'yarn' ? 'add' : 'install'
+    const res = spawn.sync(
+      pkgManager,
+      [installCommand, '-D'].concat(devDeps)
+    )
+    if (res.error) {
+      console.log('Could not execute install command, please re-run again.');
+    }
   }
 
   static async getAskEnv() {
@@ -182,9 +228,42 @@ class Initial {
     ])
   }
 
+  static async getAskPackageManager() {
+    return inquirer.prompt([
+      {
+        type: 'list',
+        name: 'pkgManager',
+        message: 'Which package manager you want to use?',
+        choices: ['npm', 'yarn', 'pnpm'],
+        default: 'npm',
+      }
+    ])
+  }
+
+  static async getAskEditorConfig() {
+    return inquirer.prompt([
+      {
+        type: 'confirm',
+        name: 'editorconfig',
+        message: 'Create .editorconfig?',
+        default: true,
+      }
+    ])
+  }
+
+  static async getAskInstall() {
+    return inquirer.prompt([
+      {
+        type: 'confirm',
+        name: 'install',
+        message: 'Install dependencies or just create config files',
+        default: false,
+      }
+    ])
+  }
+
   static generateTSDeps() {
     const { framework } = this.theAnswer
-    console.log(this.tsDepsMap[framework]);
     this.dependencies.deps = [
       ...this.dependencies.deps,
       ...this.tsDepsMap[framework].deps,
@@ -236,6 +315,40 @@ class Initial {
     if (prettier) handler(prettierWithESLint)
   }
 
+  static generateStylelintDeps() {
+    this.dependencies.devDeps = [
+      ...this.dependencies.devDeps,
+      'stylelint',
+      'stylelint-config-standard',
+      (this.theAnswer.prettier && 'stylelint-config-prettier'),
+    ]
+  }
+
+  static generatePrettierDeps() {
+    this.dependencies.devDeps.push('prettier')
+  }
+
+  // maybe run it after pkgManager install
+  static async generateHusky() {
+
+  }
+
+  static writePackageJSON() {
+    try {
+      const pkg = JSON.parse(fs.readFileSync(
+        path.resolve(process.cwd(), 'package.json'),
+        'utf-8',
+      ))
+      pkg.devDependencies = {
+        ...(pkg.devDependencies || {}),
+        ...this.dependencies.devDeps,
+      }
+      console.log(pkg);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   static writeTSConfigFile() {
     let prefix = ''
     switch (this.theAnswer.framework) {
@@ -266,21 +379,11 @@ class Initial {
         alreadyPath, { encoding: 'utf-8' }
       )
       const data = new Uint8Array(Buffer.from(alreadyConfigFile))
-      fs.writeFile('tsconfig.previous.json', data, {
-        encoding: 'utf-8',
-      }, (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-      })
+      fs.writeFileSync('tsconfig.previous.json', data, 'utf-8')
     }
 
     const libData = new Uint8Array(Buffer.from(libConfig))
-    fs.writeFile('tsconfig.json', libData, {
-      encoding: 'utf-8',
-    }, (err) => {
-      if (err) throw err
-      console.log('tsconfig.json has been saved!')
-    })
+    fs.writeFileSync('tsconfig.json', libData, 'utf-8')
   }
 
   static writeESLintConfigFile() {
@@ -341,16 +444,36 @@ class Initial {
     eslintLibConfig.plugins = plugins
     eslintLibConfig.extends = extendsList
     eslintLibConfig.rules = rules
-    console.log(eslintLibConfig);
-  }
-}
 
-function getTargetPkgJsonOnParse() {
-  return JSON.parse(
-    fs.readFileSync(pkgFilePath, {
-      encoding: "utf8",
+    const stringifiedConfig = `module.exports = ${
+      stringify(eslintLibConfig, {
+        cmp: function(a, b) {
+          return a.key > b.key ? 1 : -1
+        },
+        space: 2,
+      })
+    }\n`
+
+    fs.writeFileSync('.eslintrc.js', stringifiedConfig, 'utf-8')
+  }
+
+  static writeStylelintConfigFile() {
+    if (this.theAnswer.prettier) {
+      stylelintLibConfig.extends.push('stylelint-config-prettier')
+    }
+    const stringifiedConfig = stringify(stylelintLibConfig, {
+      space: 2,
     })
-  );
+    fs.writeFileSync('.stylelintrc.json', stringifiedConfig, 'utf-8')
+  }
+
+  static writePrettierConfigFile() {
+    fs.writeFileSync('.prettierrc.json', stringify(prettierLibConfig, { space: 2 }), 'utf-8')
+  }
+
+  static writeEditorConfigFile() {
+    fs.writeFileSync('.editorconfig', editorConfig, 'utf-8')
+  }
 }
 
 async function initAction() {
@@ -360,64 +483,8 @@ async function initAction() {
     return
   }
 
+  // Initial.writePackageJSON()
   Initial.initActions()
-  // inquirer.prompt([
-  //     {
-  //       type: 'confirm',
-  //       name: 'husky',
-  //       message: 'husky?',
-  //     },
-  //     {
-  //       type: 'confirm',
-  //       name: 'lint-staged',
-  //       message: 'lint-staged?',
-  //     },
-  //     {
-  //       type: "list",
-  //       name: "tsconfig",
-  //       message:
-  //         "tsconfig.json? Choose and install typescript or not. If your dependencies doesn't have typescript and it will be install.",
-  //       choices: ["No", "JavaScript", "Node"],
-  //       default: "No",
-  //     },
-  //   ])
-    // .then(async (answers) => {
-    //   const { tsconfig, husky } = answers;
-
-    //   if (!fs.existsSync(pkgFilePath)) {
-    //     chalk.red('This project initial not yet by npm init. Please execute command `npm init`')
-    //     return
-    //   }
-
-    //   let finalDeps = [];
-    //   let finalDevDeps = [];
-
-    //   if (husky) {
-    //     finalDevDeps = ["husky"];
-    //   }
-
-    //   let tsDeps = {};
-    //   if (tsconfig && tsconfig !== "No") {
-    //     tsDeps = getTsDependencies(tsconfig === "Node");
-    //   }
-    //   console.log(tsDeps);
-
-    //   if (Reflect.has(tsDeps, "depArgs") && tsDeps.depArgs.length) {
-    //     finalDeps = [...finalDeps, ...tsDeps.depArgs];
-    //   }
-
-    //   if (Reflect.has(tsDeps, "devDepArgs") && tsDeps.devDepArgs.length) {
-    //     finalDevDeps = [...finalDevDeps, ...tsDeps.devDepArgs];
-    //   }
-
-    //   if (finalDeps.length) {
-    //     await execa("npm", ["install", "--save", ...finalDeps]);
-    //   }
-
-    //   if (finalDevDeps.length) {
-    //     await execa("npm", ["install", "--save-dev", ...finalDevDeps]);
-    //   }
-    // });
 }
 
 module.exports = initAction;
